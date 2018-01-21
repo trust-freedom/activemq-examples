64661cfb55382c5e3f9c81c4c684ae4dd99143ff
sunli <lisun4@jieyuechina.com>
2018-01-21 19:53:03 +0800

    Consumer消费者 helloworld

--- activemq-helloworld/src/main/java/com/freedom/helloworld/Sender.java
+++ activemq-helloworld/src/main/java/com/freedom/helloworld/Sender.java
@@ -4,13 +4,14 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
 import javax.jms.Destination;
-import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
 
-public class Receiver {
+public class Sender {
     public static void main(String[] args) throws Exception {
         //第一步：建立ConnectionFactory工厂对象，需要填入用户名、密码、以及要连接的地址，均使用默认即可，默认端口号为"tcp://localhost:61616"
         ConnectionFactory ConnectionFactory = new ActiveMQConnectionFactory(
@@ -32,19 +33,24 @@ public class Receiver {
         //第四步：通过Session创建Destination对象，指的是客户端用来指定生产消息目标或消费消息来源的对象
         //        在PTP模式中，Destination被称作Queue即队列；在Pub/Sub模式中，Destination被称作Topic即主题
         //        在程序中可以使用多个Queue或Topic
-        Destination destination = session.createQueue("queue1"); //如果queue1已经存在，只是创建到queue1的Destination
+        Destination destination = session.createQueue("queue1"); //如果queue1已经存在，只会往queue1中添加消息
 
         //第五步：通过Session创建生产者或消费者(MessageProducer/MessageConsumer)，通过参数指定Destination目的地
-        MessageConsumer consumer = session.createConsumer(destination);
+        MessageProducer producer = session.createProducer(destination);
 
-        //第六步：消费者不需要设置是否持久化
-        //producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        //第六步：可以使用MessageProducer的setDeliveryMode()方法为其设置持久化特性和非持久化特性（DeliveryMode）
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT); //可以针对单个生产者设置是否持久化
 
-        //第七步：循环消费消息
-        while (true){
-            TextMessage msg = (TextMessage)consumer.receive();  //阻塞的接收消息
-            if(msg == null) break;
-            System.out.println("收到的内容：" + msg.getText());
+        //第七步：最后通过Session创建JMS规范中TextMessage形式的数据，并用MessageProducer的send()方法发送数据
+        //        同理，使用receive()方法接收数据
+        //        最后不要忘记关闭Connection连接
+        for(int i=1; i<=5; i++){
+            TextMessage textMessage = session.createTextMessage();
+            textMessage.setText("我是消息内容，id为：" + i);
+
+            producer.send(textMessage);
+            //producer.send(destination, textMessage);  //也可以在send消息时指定目的地
+            System.out.println("生产者：" + textMessage.getText());
         }
 
         if(connection != null){
